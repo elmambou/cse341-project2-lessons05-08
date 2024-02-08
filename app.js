11cd546498d6b1ded7f871ec42a51faa3df8b638
elmambou <esmambou@gmail.com>
2024-02-08 16:56:44 +0200

    updating changes to app.js

--- app.js
+++ app.js
@@ -6,40 +6,28 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
 const app = express();
 
-const authorRoutes = require('./routes/author');
-const bookRoutes = require('./routes/book');
 
-
-
-// Enable CORS
+app.use(bodyParser.json());
 app.use(cors());
 
-// Parse JSON bodies
-app.use(bodyParser.json())
-   .use((req, res, next) => {
-     res.setHeader('Access-Control-Allow-Origin', '*');
-     res.setHeader('Access-Control-Allow-Headers',
-     'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
-     res.setHeader('Content-Type', 'application/json');
-     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
- next();
-   })
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers',
+  'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+})
+ .use('/', require('./routes'));
 
-// Routes
-mongodb.initDb()
-    .then(() => {
-        app.locals.db = mongodb.getDb();
 
-        app.use('/', require('./routes'));
-        app.use('/author', authorRoutes);
-        app.use('/book', bookRoutes);
-    
-app.listen(port, () => {
-    console.log(`Connected to DB and listening on ${port}`);
-    console.log(`http://localhost:${port}/author`);
-    console.log(`http://localhost:${port}/book`);
-        });
-    })
-    .catch((err) => {
-        console.error('Error starting the app:', err);
-    });
+mongodb.initDb((err) => {
+    if (err) {
+      console.log(err);
+      // Respond with an error message to the client
+    }else {
+     app.listen(port);
+     console.log(`Connected to DB and listening on ${port}`);
+    }
+});
+  
