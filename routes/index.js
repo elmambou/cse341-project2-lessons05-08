8e6cdec259d7b67e2a2a7d9b620c4c68d2ec1690
elmambou <esmambou@gmail.com>
2024-02-08 15:32:40 +0200

    adding swagger functionality to lesson 05

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 
-//router.use('/', require('./swagger'));
+router.use('/', require('./swagger'));
 router.use('/author', require('./author'))
 router.use('/book', require('./book'))
 
---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Bookstore API'
+  },
+  host: 'localhost:8080', 
+  schemes: ['http'],      // Add a comma here
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+// generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
