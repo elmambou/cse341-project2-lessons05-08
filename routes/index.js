64e1a020082d59f88bd9809c6790eb18c666ebd4
elmambou <esmambou@gmail.com>
2024-02-21 21:45:48 +0200

    changes to lesson 07

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,14 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/', require('./swagger'));
-router.use('/author', require('./author'))
-router.use('/book', require('./book'))
+router.use('/author', require('./author'));
+router.use('/book', require('./book'));
+
+
+
+//NEW LINES TO ROUTES/INDEX.JS  3
+router.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 module.exports = router;
