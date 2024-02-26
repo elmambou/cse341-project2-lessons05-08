f08ce13113cb2227330c26517718ed5cab4a7947
elmambou <esmambou@gmail.com>
2024-02-26 21:18:24 +0200

    adding changes to lesson 08

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cse341-project2-lessons05-08
+
+authorize.js now retrieves the configuration values from environment variables.
+app.js retrieves the configuration information from the config module, which in turn reads the values from environment variables.
+.env file contains all the sensitive information and configuration variables.
+db/config.js is left untouched as it is a separate concern and not directly related to the configuration used in authorize.js or app.js.
+
--- app.js
+++ app.js
@@ -2,20 +2,60 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongodb = require('./db/connect');
-const { auth, requiresAuth } = require('express-openid-connect');
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.SESSION_SECRET,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
-};
+const config = require('./config'); // config path 
+
+//graphQL
+const { graphqlHTTP } = require('express-graphql'); // Corrected import statement
+const { ObjectId } = require('mongodb'); //Import ObjectId
+const schema = require('./schema');
 
 
 const port = process.env.PORT;
 const app = express();
+
+
+// Example usage of ObjectId
+app.get('/author/:id', (req, res) => {
+  const id = req.params.id;
+  const objectId = new ObjectId(id);
+  // Now you can use objectId to perform operations with MongoDB
+  // For example, querying a document by its ID
+  // Example: db.collection('author').findOne({ _id: objectId });
+  res.send('author ID: ' + id);
+});
+
+// Example usage of ObjectId
+app.get('/book/:id', (req, res) => {
+  const id = req.params.id;
+  const objectId = new ObjectId(id);
+  // Now you can use objectId to perform operations with MongoDB
+  // For example, querying a document by its ID
+  // Example: db.collection('book').findOne({ _id: objectId });
+  res.send('book ID: ' + id);
+});
+
+
+//End of ObjectId usage
+
+
+
+//This route will be used as an endpoint to interact with Graphql, 
+//All queries will go through this route. 
+app.use('/graphql', graphqlHTTP((req) => ({
+  schema,
+  graphiql: true,
+  context: { db: app.locals.db }, // Pass the MongoDB connection to the context
+})));
+
+
+//End of GraphQL
+
+
+const { auth, requiresAuth } = require('express-openid-connect');
+const authorRoutes = require('./routes/author');
+const bookRoutes = require('./routes/book');
+const authorizeRoutes = require('./routes/authorize');
 
 
 app.use(auth(config));
@@ -29,7 +69,6 @@ app.get('/profile', requiresAuth(), (req, res) => {
   res.send(JSON.stringify(req.oidc.user));
 });
 
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
@@ -37,12 +76,12 @@ app.use((req, res, next) => {
   next();
 });
 
-const authorRoutes = require('./routes/author');
-const bookRoutes = require('./routes/book');
+
+
 app.use('/', require('./routes'));
 app.use('/author', authorRoutes);
 app.use('/book', bookRoutes);
-
+app.use('/authorize', authorizeRoutes); // Use the authorization routes here
 
 process.on('uncaughtException', (err, origin) => {
   console.error(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
